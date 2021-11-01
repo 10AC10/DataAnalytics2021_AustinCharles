63a7a9efa99c82e3e3f673c0d9d0e6129af86064
10AC10 <90467013+10AC10@users.noreply.github.com>
2021-11-01 10:30:00 -0400

    Add files via upload

--- Titanic_Trees.R
+++ Titanic_Trees.R
@@ -11,7 +11,7 @@ levels(Titanic$Survived) <- c(1,0)
 
 
 
-Titanic_rpart <- rpart(class ~ Freq + Survived + Age, data = Titanic)
+Titanic_rpart <- rpart(Survived ~ Freq + Class + Age, data = Titanic)
 plot(Titanic_rpart) # try some different plot options
 text(Titanic_rpart) # try some different text options
 
@@ -21,7 +21,7 @@ text(Titanic_rpart) # try some different text options
 # Regression Tree 
 require(rpart)
 # build the  tree
-fitT <- rpart(class ~ Freq + Survived + Age, method="anova", data=Titanic)
+fitT <- rpart(Survived ~ Freq + Class + Age, method="anova", data=Titanic)
 printcp(fitT) # display the results
 plotcp(fitT) #margins to big
 summary(fitT)
@@ -36,3 +36,34 @@ pfitT<- prune(fitT, cp=0.01160389) # from cptable??? adjust this to see the effe
 plot(pfitT, uniform=TRUE, main="Pruned Regression Tree for Titanic")
 text(pfitT, use.n=TRUE, all=TRUE, cex=.8)
 post(pfitT, file = ptree2.ps, title = "Pruned Regression Tree for Titanic")
+
+#Titanic ctree
+View(Titanic)
+library(tree)
+tr <- tree(Survived ~ ., data=Titanic)
+tr
+tr$frame
+plot(tr)
+text(tr, cex=0.5)
+#figure margins to large??
+
+fit2T <- ctree(Survived ~ Freq + Class + Age, data = na.omit(Titanic))
+summary(fit2T)
+# plot tree
+plot(fit2T, uniform=TRUE, main="CI Tree Tree for Titanic ")  #invalid graphics state
+text(fit2T, use.n=TRUE, all=TRUE, cex=.8)
+
+
+fitTK <- ctree(Survived ~ Freq + Class + Age, data=Titanic)
+plot(fitTK, main="Conditional Inference Tree for Titanic")
+plot(fitTK, main="Conditional Inference Tree for Titanic",type="simple")
+
+#error in call graphics
+
+#hclust
+
+t <- dist(as.matrix(Titanic))   
+tc <- hclust(t)               
+plot(tc)
+
+#how to use variables since can't $ attach
